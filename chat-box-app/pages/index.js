19539c98d647994696826b77938355213c3dbfe5
Angelo Nino <angelonino38@gmail.com>
2021-03-04 00:50:14 +0800

    changes

--- chat-box-app/pages/index.js
+++ chat-box-app/pages/index.js
@@ -18,14 +18,7 @@ export default function Home() {
       </main>
 
       <footer>
-        Powered by
-        <a href="https://vercel.com" target="_blank" rel="noopener noreferrer">
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-        and
-        <a href="https://ably.com" rel="noopener noreferrer">
-          <img src="/ably-logo.svg" alt="Ably Logo" className="logo ably" />
-        </a>
+        
       </footer>
 
       <style jsx>{`
